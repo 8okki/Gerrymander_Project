0d8ee86b52f699a3c122f71ecd53b19e9500389f
Edward Tsang <edward.tsang99@gmail.com>
2019-11-13 04:00:42 -0500

    Fill in remaining misc functions

--- server/src/main/java/com/cse308/server/gerrymander/Precinct.java
+++ server/src/main/java/com/cse308/server/gerrymander/Precinct.java
@@ -34,7 +34,7 @@ public class Precinct {
     public Map<Demographic, Integer> getDemogrphicPop(Demographic[] demographics){
         HashMap<Demographic, Integer> output = new HashMap<Demographic, Integer>();
         for(int i = 0; i < demographics.length; i++){
-            for(Map.Entry<Demographic, Integer> entry : this.demographicPopDist.entrySet()){
+            for(Map.Entry<Demographic, Integer> entry : this.demographicPopulationDist.entrySet()){
                 if (demographics[i].equals(entry.getKey())){
                     output.put(entry.getKey(),entry.getValue());
                 }
@@ -44,9 +44,9 @@ public class Precinct {
     }
     
     private Demographic findLargestDemographic(){
-        int curLargest;
-        Demographic curDemo;
-        for(Map.Entry<Demographic, Integer> entry : this.demographicPopDist.entrySet()){
+        int curLargest = Integer.MAX_VALUE;
+        Demographic curDemo = null;
+        for(Map.Entry<Demographic, Integer> entry : this.demographicPopulationDist.entrySet()){
             if (entry.getValue() > curLargest){
                 curLargest = entry.getValue();
                 curDemo = entry.getKey();
