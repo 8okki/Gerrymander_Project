e203c839c792905a8b407497f94f3270cc283121
dumdo <jejeine@gmail.com>
2019-11-12 23:47:48 -0500

    added controller

---
+++ server/src/main/java/cse308/server/controller/HomeController.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cse308.server.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;    
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Jakob
+ */
+
+@RestController
+public class HomeController {
+    @GetMapping("/home")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting " + name;
+    }
+    @PostMapping("/greeting")
+    public String greetingPost(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greetings "  + name;
+    }
+}
