6392c1b39f6d3593446ebe0c2a7ed476f1a73dff
dumdo <jejeine@gmail.com>
2019-12-15 19:37:50 -0500

    loaded states are now backed up, so support for multiple states is closer (need to make precodes unique)

--- server/src/main/java/com/cse308/server/algorithm/Algorithm.java
+++ server/src/main/java/com/cse308/server/algorithm/Algorithm.java
@@ -18,9 +18,11 @@ import com.cse308.server.hibernate.dao.StateDao;
 import com.cse308.server.models.State;
 import com.cse308.server.result.Phase1Result;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,6 +42,8 @@ public class Algorithm {
     
     State state;
 
+    Map<StateName,State> loadedStates;
+    
     boolean incrementalRunning;
     
     public DistrictInfo getDistrictInfo(int districtId, Demographic[] demographics){
@@ -48,10 +52,14 @@ public class Algorithm {
 
     /* Initialize */
     public State initState(StateName stateName){
-        if(state == null || StateName.valueOf(state.getName()) != stateName){
+        if(loadedStates == null){
+            loadedStates = new HashMap<>();
+        }
+        if(state == null || !loadedStates.containsKey(StateName.valueOf(state.getName()))){
             State result = stateDao.getStateById(stateName.name());
             if(result != null){
                 state = result;
+                loadedStates.put(stateName, state);
                 return state;
             }else{
                 return null;
